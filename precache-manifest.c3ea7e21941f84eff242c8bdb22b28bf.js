a0d85d9f0020970a9dc6e6fd37b3c78e02882965
Yegor <ligoudnd@gmail.com>
2020-08-31 04:06:14 +0400

    Updates

---
+++ precache-manifest.c3ea7e21941f84eff242c8bdb22b28bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbe7effb27075d9e3056614db95629f",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "a0b5ff90b63b25300de1",
+    "url": "/CV/static/css/main.0c790d53.chunk.css"
+  },
+  {
+    "revision": "c401bafd5afcc0260087",
+    "url": "/CV/static/js/2.ba2ac6cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV/static/js/2.ba2ac6cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b5ff90b63b25300de1",
+    "url": "/CV/static/js/main.0f625e75.chunk.js"
+  },
+  {
+    "revision": "e4d273214469930d29bd",
+    "url": "/CV/static/js/runtime-main.4d0d12ac.js"
+  },
+  {
+    "revision": "6e91881354d4ea0a1795abdba7c26c4b",
+    "url": "/CV/static/media/Pines&Mountains.6e918813.svg"
+  }
+]);
